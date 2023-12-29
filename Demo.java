c7cf14ad2da19b250ff61436c35da82157f85fe1
abhi7pute <abhijeetsatpute9817@gmail.com>
2023-12-29 22:26:24 +0530

    Demo File Will be changed !

--- Demo.java
+++ Demo.java
@@ -1,5 +1,7 @@
 class Demo{
     public class m1(){
-        System.out.println("Here is the rutu s Demo file");
+        System.out.println("Here is the rutu s Demo file"); 
+        System.out.println("Here is the rutu s Demo file"); 
+
     }
 }
